e365580516ad79faa010e69bf6f96860a27919e4
kumail4real <Kumailali047@gmail.com>
2023-08-06 00:48:27 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+console.log("hy kumail");
+var userName = "kumail kazmi";
+console.log(userName);
+var numb = 5;
+var num1 = 8;
+var result = (numb * num1);
+console.log(result);
+var num2 = 2;
+var num3 = 4;
+var result = (numb + num1 / num3);
+console.log(result * num2);
+var $1 = 10;
+var $2 = 20;
+var result = $1 * $2;
+console.log(result);
---
+++ index.ts
@@ -0,0 +1,11 @@
+console.log("hy kumail");
+var userName:string = "kumail kazmi";
+console.log(userName);
+var numb:number = 5;
+var num1:number = 8;
+var result:number = (numb*num1);
+console.log(result);
+var num2:number = 2
+var num3:number = 4
+var result:number = (numb+num1/num3);
+console.log(result*num2);
